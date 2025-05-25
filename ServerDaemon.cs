7e170fe61373cf488c954381d1223c441bb18792
m9 studio <m9.agrest@gmail.com>
2025-05-25 19:47:03 +0300

    update daemon

--- ServerDaemon.cs
+++ ServerDaemon.cs
@@ -12,19 +12,55 @@ namespace M9Studio.ShadowTalk.Server
             while (true)
             {
                 JObject packet = session.ReceiveJObject();
-                //todo обработка
-
+                try
+                {
+                    SearchUser(session, PacketStruct.Parse<PacketClientToServerSearchUser>(packet));
+                    continue;
+                } catch (Exception ex) { }
+                try
+                {
+                    SendMessage(session, PacketStruct.Parse<PacketClientToServerSendMessage>(packet));
+                    continue;
+                }
+                catch (Exception ex) { }
+                try
+                {
+                    GetUser(session, PacketStruct.Parse<PacketClientToServerGetUser>(packet));
+                    continue;
+                }
+                catch (Exception ex) { }
+                try
+                {
+                    ConnectP2P(session, PacketStruct.Parse<PacketClientToServerConnectP2P>(packet));
+                    continue;
+                }
+                catch (Exception ex) { }
+                //Console.WriteLine("Error");
             }
         }
         private void SearchUser(SecureSession<IPEndPoint> session, PacketClientToServerSearchUser packet)
         {
-
-        }
-        private void SendMessage(SecureSession<IPEndPoint> session, PacketClientToServerSendMessage packet)
-        {
-
+            List<User> users = @base.Users("SELECT * FROM users where name LIKE LIMIT 5", $"%{packet.Name}%");
+            PacketServerToClientAnswerOnSearchUser answer = new PacketServerToClientAnswerOnSearchUser()
+            {
+                Ids = users.Select(x => x.Id).ToArray(),
+                Names = users.Select(x => x.Name).ToArray(),
+                RSAs = users.Select(x =>x.RSA).ToArray()
+            };
+            session.Send(answer);
         }
         private void GetUser(SecureSession<IPEndPoint> session, PacketClientToServerGetUser packet)
+        {
+            List<User> users = @base.Users("SELECT * FROM users where id = ?", packet.Id);
+            PacketServerToClientAnswerOnSearchUser answer = new PacketServerToClientAnswerOnSearchUser()
+            {
+                Ids = users.Select(x => x.Id).ToArray(),
+                Names = users.Select(x => x.Name).ToArray(),
+                RSAs = users.Select(x => x.RSA).ToArray()
+            };
+            session.Send(answer);
+        }
+        private void SendMessage(SecureSession<IPEndPoint> session, PacketClientToServerSendMessage packet)
         {
 
         }
