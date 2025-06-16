6af052175ba1ea7eff63e2f0b7b9d5c13b6ea17b
m9 studio <m9.agrest@gmail.com>
2025-06-17 01:15:55 +0300

    Update FormMain.cs

--- FormMain.cs
+++ FormMain.cs
@@ -67,9 +67,9 @@ namespace M9Studio.ShadowTalk.Server
         }
         private void Save(string nick, string login, string password)
         {
-            int id = server.@base.Count("SELECT MAX(id) AS num FROM users") + 1;
+            int id = server.@base.Count("SELECT IFNULL(MAX(id), 0) AS num FROM users") + 1;
             string RSA = "<RSAKeyValue><Modulus>t6hxT8GPOvYBdUmtt6M8zVguGu9XTIjG+EO7nWlMNQLFDgU6Aksa8/YTM1puetcr8xsX0EO9ZxJqrlb19JogyEO1W3HmPYd/mI6VfXEYPlAB32XGD3lTb3DjZ+DEJ6HyIT0YsKG9YUztebuUGk6ZvheXOp0fByn1oQbRrIGjJZLh9ZfXeqii0yHiKWyyxoUPqkEvGwNABXgKjvHtYODiyVxOfATFBS+1m6uRPgm5/cDSEBJ72GvMHAwE1bx+UeNDmOvXBP0Kn/xqQwFUQHwcjBdKYw+iwQI0oVkVLJkJn7PU7vEbL7Kp9ed1Rky4OR5zJVIaUT998Rjf8+8iDuUHHQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
-            server.@base.Send("INSERT INTO users (id, name, email, password, rsa) VALUES (?, ?, ?, ?, ?)", id, nick, login, password, RSA);
+            server.@base.Send("INSERT INTO users (id, name, email, password, rsa, is2fa) VALUES (?, ?, ?, ?, ?, 0)", id, nick, login, password, RSA);
             server.Update();
         }
     }
@@ -102,9 +102,11 @@ namespace M9Studio.ShadowTalk.Server
 
             var labelLogin = new Label() { Text = "Логин:", Left = 10, Top = 60 };
             textLogin = new TextBox() { Left = 120, Top = 60, Width = 150 };
+            textLogin.Text = "@com.com";
 
             var labelPassword = new Label() { Text = "Пароль:", Left = 10, Top = 100 };
             textPassword = new TextBox() { Left = 120, Top = 100, Width = 150, PasswordChar = '*' };
+            textPassword.Text = "00000000";
 
             btnSave = new Button() { Text = "Сохранить", Left = 100, Top = 140, Width = 100 };
             btnSave.Click += BtnSave_Click;
